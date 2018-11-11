2d50cd9974f2f03f5440c05582c75ab0fd95ee66
navipl <ivanplucas@hotmail.com>
2018-11-11 16:08:16 -0200

    11.11.18

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ function MostrarBox (d) {
     $('.cont-principal').children().removeClass('ativo');
 $(d).addClass("ativo");
 }
+
+function ManipilaMoldal(){
+   $("#Moldal").toggleClass('moldal-ativo');
+   $("#Moldal").toggleClass('moldal-inativo');
+}
+
+function ExibeMoldal(m){
+    $("#Moldal .cont-moldal div").empty();
+
+    var conteudo = $(m).html();
+    $("#Moldal .cont-moldal div").append(conteudo);
+
+    ManipilaMoldal();
+}
